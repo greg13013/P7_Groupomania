b17b8009a7f74b2843e619e5803d815a7a295e38
Colli Gregory <colli.gregory.13@gmail.com>
2022-03-14 18:19:19 +0100

    modif config

--- backend/config/db.config.js
+++ backend/config/db.config.js
@@ -1,28 +1,28 @@
-// module.exports = {
-//     HOST: "localhost",
-//     USER: "root",
-//     PASSWORD: "root",
-//     DB: "groupomania",
-//     dialect: "mysql",
-//     pool: {
-//       max: 5,
-//       min: 0,
-//       acquire: 30000,
-//       idle: 10000
-//     }
-//   };
+module.exports = {
+    HOST: "localhost",
+    USER: "root",
+    PASSWORD: "root",
+    DB: "groupomania",
+    dialect: "mysql",
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    }
+  };
 
 //SITE EN LIGNE
-module.exports = {
-  HOST: "eu-cdbr-west-02.cleardb.net",
-  USER: "b225a9ad0874bf",
-  PASSWORD: "9f1414a6",
-  DB: "heroku_86d2b01f8c98d97",
-  dialect: "mysql",
-  pool: {
-    max: 90,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
-  }
-};
+// module.exports = {
+//   HOST: "eu-cdbr-west-02.cleardb.net",
+//   USER: "b225a9ad0874bf",
+//   PASSWORD: "9f1414a6",
+//   DB: "heroku_86d2b01f8c98d97",
+//   dialect: "mysql",
+//   pool: {
+//     max: 90,
+//     min: 0,
+//     acquire: 30000,
+//     idle: 10000
+//   }
+// };
--- frontend/src/services/config.js
+++ frontend/src/services/config.js
@@ -3,20 +3,20 @@ import cookie from 'js-cookie'
 
 
 
-// export default axios.create({
-//   baseURL: "http://localhost:3001",
-//   headers: {
-//     "Content-type": "application/json"
-//   }
-// });
-
-//Config deploiement netlify
 export default axios.create({
-  baseURL: "https://colligroupomania.herokuapp.com",
+  baseURL: "http://localhost:3001",
   headers: {
     "Content-type": "application/json"
   }
 });
+
+//Config deploiement netlify
+// export default axios.create({
+//   baseURL: "https://colligroupomania.herokuapp.com",
+//   headers: {
+//     "Content-type": "application/json"
+//   }
+// });
 
 export function getToken(){
     return cookie.get('jwt');
